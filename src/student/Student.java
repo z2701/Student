778cff045a1b0a0f43cc9fb87ac9c275e2b7b7bb
z2701 <malikzu@sheridancollege.ca>
2021-01-29 11:02:14 -0500

    This is First Commit

---
+++ src/student/Student.java
@@ -0,0 +1,31 @@
+
+
+package student;
+
+/**
+ *
+ * This class +++Insert Description Here+++
+*
+*@author Zunaira Malik
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        
+        private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+     public static void main(String[]args){
+      
+
+}
+}
---
+++ src/student/StudentList.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author zunai
+ */
+public class StudentList {
+    public static void main(String[]args){
+        Student[] studentList= new Student[2]; //craet aaray
+        Student s1 =new Student();
+        s1.setName("Zunaira");
+        Student s2 =new Student();
+        s2.setName("Malik");
+        studentList[0]=s1;
+        studentList[1]=s2;
+        for (Student s : studentList){
+            System.out.println(s.getName());
+            
+        }
+    }
+    
+}
